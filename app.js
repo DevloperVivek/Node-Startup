58e5c08a7678ffb0c5c788dfc62e97a8dc04fb7e
thevivekraut <vraut2306@gmail.com>
2023-09-03 11:27:41 +0530

    URL Based Response

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request);
-  console.log("Vivek");
+  const url = request.url;
+
+  response.setHeader("Content-Type", "text/html");
+  if (url === "/home") {
+    response.write("<html><head><body>Welcome home</body></head></html>");
+    response.end();
+  } else if (url === "/about") {
+    response.write(
+      "<html><head><body>Welcome to About Us page</body></head></html>"
+    );
+    response.end();
+  } else if (url === "/node") {
+    response.write(
+      "<html><head><body>Welcome to my Node Js project</body></head></html>"
+    );
+    response.end();
+  }
 });
 
-server.listen(4000);
+server.listen(3000);
