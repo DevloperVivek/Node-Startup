2e9391676ce1c481f49c6e4f90bf13e9311088c0
thevivekraut <vraut2306@gmail.com>
2023-09-03 14:36:40 +0530

    Reading and Writing File

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((request, response) => {
   const url = request.url;
+  const method = request.method;
 
-  response.setHeader("Content-Type", "text/html");
-  if (url === "/home") {
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(data);
+      response.write("<html>");
+      response.write("<head><title>Enter Message</title></head>");
+      response.write(`<body>${data}</body>`);
+      response.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      response.write("</html>");
+      return response.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    request.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return request.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, () => {
+        response.statusCode = 302;
+        response.setHeader("Location", "/");
+        return response.end();
+      });
+    });
+  } else {
+    response.setHeader("Content-Type", "text/html");
     response.write("<html><head><body>Welcome home</body></head></html>");
-    response.end();
-  } else if (url === "/about") {
-    response.write(
-      "<html><head><body>Welcome to About Us page</body></head></html>"
-    );
-    response.end();
-  } else if (url === "/node") {
-    response.write(
-      "<html><head><body>Welcome to my Node Js project</body></head></html>"
-    );
     response.end();
   }
 });
